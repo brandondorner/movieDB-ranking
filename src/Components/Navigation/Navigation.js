0fc195a52dab30b188cb3c0f6d8b6bb06ae1d4da
brandondorner <51007432+brandondorner@users.noreply.github.com>
2019-11-01 16:18:29 -0500

    cleaned uo creat react app, added nav component

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./nav.css"
+
+const Navigation = () => {
+    return (
+        <nav>
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation;
