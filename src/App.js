7249749db0412acdee99d3671d802b6c49dd4551
brandondorner <51007432+brandondorner@users.noreply.github.com>
2019-11-04 16:53:36 -0600

    cleaned up conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from "./Components/Navigation/Navigation";
 import SignIn from "./Components/SignIn/SignIn";
+import Register from './Components/Register/Register'
 import Logo from "./Components/Logo/Logo"
 import Query from "./Components/Query/Query"
 import MovieResults from "./Components/MovieResults/MovieResults"
@@ -26,37 +27,64 @@ class App extends React.Component {
   constructor(){
     super()
     this.state = {
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: 'false'
     }
   }
 
   //this function when ran will change the route state
   onRouteChange = (newRoute) => {
+    //changes signed in or signed out state
+    if (newRoute === 'signout' ){
+      this.setState({isSignedIn: false})
+    }else if (newRoute === 'home'){
+      this.setState({isSignedIn: true})
+    }
+
+    //changes route
     this.setState({route : newRoute});
   }
 
 
   render(){
-    return (
-      <div className="app">
-        <Particles className='particles'
-          params ={particlesParams}
-        />
-        <Navigation onRouteChange={this.onRouteChange} />
-
-        {/* setting up conditional of being signed in */}
-        {/* if route is sign in, then render signin. if not, render everything else */}
-        {/* SignIn passes down prop onRouteChange */}
-        { this.state.route === "signin"
-        ? <SignIn onRouteChange={this.onRouteChange}/>
-        : <div>
+    
+    //     setting up conditional of being signed in 
+    //     if route is sign in, then render signin. same for home, other, etc
+    //     passes down prop onRouteChange 
+    if (this.state.route === 'home'){
+      return(
+        <div className="app">
+          <Particles className='particles'
+            params ={particlesParams} />
+          <Navigation onRouteChange={this.onRouteChange} />
+          <div>
             <Logo />
             <Query />
             <MovieResults />
           </div>
-        }
-      </div>
-    );
+        </div>
+      )
+
+
+    }else if (this.state.route === "signin"){
+      return (
+        <div className="app">
+            <Particles className='particles'
+              params ={particlesParams}/>
+            <SignIn onRouteChange={this.onRouteChange}/>
+         </div>
+      )
+
+
+    }else{
+      return (
+        <div className="app">
+          <Particles className='particles'
+            params ={particlesParams}/>
+          <Register onRouteChange={this.onRouteChange} />
+        </div>
+      )
+    }
   }
  
 }
